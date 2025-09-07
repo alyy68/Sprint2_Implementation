ebc48d2d5f8af80e8181993051a5b4a8bb25e17e
aliti <alitipirdamaz0@gmail.com>
2025-09-07 19:51:55 +0300

    Project creation

---
+++ src/test/java/com/sprint2_implementation/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.sprint2_implementation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/sprint2_implementation/step_definitions",
+        dryRun = false,
+        tags = "@wip",
+        publish = true //generating a report with public link
+)
+public class CukesRunner {}
+
+
---
+++ src/test/java/com/sprint2_implementation/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.sprint2_implementation.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/sprint2_implementation/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
