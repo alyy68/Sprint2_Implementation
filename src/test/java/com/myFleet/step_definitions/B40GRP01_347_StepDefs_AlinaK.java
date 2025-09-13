90a93c28310d8edd71c2cf86a92fd5f29d6dc1d6
AlinaKhrystiuk <sachuk.alna@gmail.com>
2025-09-13 22:22:06 +0300

    B40GRP01-346 already done, all Test Cases is PASSED

--- src/test/java/com/myFleet/step_definitions/B40GRP01_347_StepDefs_AlinaK.java
+++ src/test/java/com/myFleet/step_definitions/B40GRP01_347_StepDefs_AlinaK.java
@@ -1,11 +1,9 @@
 package com.myFleet.step_definitions;
 
-import com.fasterxml.jackson.databind.ser.std.ClassSerializer;
-import com.myFleet.pages.B40GRP01_347_CalendarPage_AlinaK;
+import com.myFleet.pages.CalendarPage_AlinaK;
 import com.myFleet.pages.BasePage;
 import com.myFleet.utilities.BrowserUtils;
 import com.myFleet.utilities.Driver;
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
@@ -17,7 +15,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class B40GRP01_347_StepDefs_AlinaK {
-    B40GRP01_347_CalendarPage_AlinaK b40GRP01347CalendarPage = new B40GRP01_347_CalendarPage_AlinaK();
+    CalendarPage_AlinaK calendarPageAlinaK = new CalendarPage_AlinaK();
     BasePage basePage=new BasePage();
     WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
 
@@ -26,16 +24,16 @@ public class B40GRP01_347_StepDefs_AlinaK {
     public void goTo_AK() {
        basePage.navigateToModule("Activities", "Calendar Events");
        basePage.waitUntilLoaderScreenDisappear();
-       b40GRP01347CalendarPage.createCalendarRepeat();
+       calendarPageAlinaK.createCalendarRepeat();
     }
 
     @Then("If enter {string} less than {int} —> users should see “The value have not to be  less than {int}.”")
     public void ifEnterLessThanUsersShouldSeeTheValueHaveNotToBeLessThan_AK(String number1, int minimum, int errorNum1) {
-        b40GRP01347CalendarPage.inputField.sendKeys(Keys.BACK_SPACE);
-        b40GRP01347CalendarPage.inputField.sendKeys(number1+Keys.ENTER);
-        wait.until(ExpectedConditions.visibilityOf(b40GRP01347CalendarPage.errorMessage));
+        calendarPageAlinaK.inputField.sendKeys(Keys.BACK_SPACE);
+        calendarPageAlinaK.inputField.sendKeys(number1+Keys.ENTER);
+        wait.until(ExpectedConditions.visibilityOf(calendarPageAlinaK.errorMessage));
 
-        String actualMessage =b40GRP01347CalendarPage.errorMessage.getText();
+        String actualMessage = calendarPageAlinaK.errorMessage.getText();
         String expectedMessage = "The value have not to be less than " +errorNum1+".";
 
         if (Integer.valueOf(number1)<minimum){
@@ -46,11 +44,11 @@ public class B40GRP01_347_StepDefs_AlinaK {
 
     @Then("If enter {string} more than {int} —> users should see “The value have not to be more than {int}.”")
     public void ifEnterMoreThanUsersShouldSeeTheValueHaveNotToBeMoreThan_AK(String number2, int maximum, int errorNum2) {
-        b40GRP01347CalendarPage.inputField.sendKeys(Keys.BACK_SPACE);
+        calendarPageAlinaK.inputField.sendKeys(Keys.BACK_SPACE);
 
-        b40GRP01347CalendarPage.inputField.sendKeys(number2+Keys.ENTER);
+        calendarPageAlinaK.inputField.sendKeys(number2+Keys.ENTER);
 
-        String actualMessage =b40GRP01347CalendarPage.errorMessage.getText();
+        String actualMessage = calendarPageAlinaK.errorMessage.getText();
         String expectedMessage = "The value have not to be more than " +errorNum2+".";
 
         if (Integer.valueOf(number2)>maximum){
