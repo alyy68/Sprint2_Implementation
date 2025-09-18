8a2104136ccd52e2200f4b90433ddefa443e8837
niloofarsarvari <n.sarvari97@gmail.com>
2025-09-18 18:42:28 +0330

    Edit Car Icon feature is added DO NOT MERGE

--- src/test/java/com/myFleet/runners/CukesRunner.java
+++ src/test/java/com/myFleet/runners/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/myFleet/step_definitions",
         dryRun = false,
-        tags = "@alina",
+        tags = "@NS",
         publish = true //generating a report with public link
 )
 public class CukesRunner {
